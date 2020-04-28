2a592f5b12d0b31d8f9431663eaa879721bf61d3
Jenna Goldman <jenna.goldman89@gmail.com>
2020-04-28 16:44:51 -0700

    setup User container component

---
+++ src/containers/User/User.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import UsernameInput from '../../components/UsernameInput.jsx';
+import UserInfo from '../../components/UserInfo.jsx';
+
+
+export default class User extends Component {
+  render() {
+    return (
+      
+    )
+  }
+}
