8b09e82473c31dfc6f9739c466d761980ba3bf20
Jenna Goldman <jenna.goldman89@gmail.com>
2020-04-28 15:23:51 -0700

    fix fetchUser

--- src/services/github.js
+++ src/services/github.js
@@ -1,3 +1,4 @@
 export const fetchUser = (username) => {
-  return fetch(`https://api.github.com/users/${username}`);
+  return fetch(`https://api.github.com/users/${username}`)
+    .then(res => res.json());
 };
