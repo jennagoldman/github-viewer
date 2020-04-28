9cb6b9c1f61a173a8992e74e1457edd5b0558cae
Jenna Goldman <jenna.goldman89@gmail.com>
2020-04-28 16:57:10 -0700

    completed User container component

--- src/containers/User/User.jsx
+++ src/containers/User/User.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import UsernameInput from '../../components/UsernameInput.jsx';
 import UserInfo from '../../components/UserInfo.jsx';
+import fetchUser from '../../services/github.js';
 
 
 export default class User extends Component {
+  state = {
+    name: '',
+    followerCount: '',
+    profileLink: ''
+  }
+
+  handleUsernameChange = ({ target }) => {
+    fetchUser(target.value)
+      .then(user => {
+        this.setState({ name: user.name });
+        this.setState({ followerCount: user.followers });
+        this.setState({ profileLink: user.html_url });
+      });
+  };
+
   render() {
+    const { username, name, followerCount, profileLink } = this.state;
     return (
-      
-    )
+      <>
+        <UsernameInput username={username} onUsernameChange={this.handleUsernameChange} />
+        <UserInfo name={name} followerCount={followerCount} profileLink={profileLink} />
+      </>
+    );
   }
 }
